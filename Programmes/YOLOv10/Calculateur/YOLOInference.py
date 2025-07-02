838835a4df4501ca55a27e8cda65b26861d2e3ce
Esteban CARRASCO <esteban.carrasco.pr@gmail.com>
2025-07-02 11:55:05 +0200

    commit2025-07-02 11:55:05

--- Programmes/YOLOv10/Calculateur/YOLOInference.py
+++ Programmes/YOLOv10/Calculateur/YOLOInference.py
@@ -1,12 +1,3 @@
-import torch
-from torch.serialization import add_safe_globals
-from ultralytics.nn.tasks import DetectionModel
-from torch.nn.modules.container import Sequential
-from ultralytics.nn.modules.conv import Conv  # <-- import de la classe Conv
-
-# Ajouter toutes les classes nécessaires
-add_safe_globals([DetectionModel, Sequential, Conv])
-
 from ultralytics import YOLO
 
 model = YOLO('Calculateur/Modèles/best001.pt', task='detect')
